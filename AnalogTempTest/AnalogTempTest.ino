2b7428be1f0ab97debfbb608f858c1f6afce1359
Alex Pieprzycki <alexpieprzycki@Alexs-MacBook-Pro.local>
2016-02-02 13:50:33 -0600

    Initail code, reading the wrong temperature, seems to be taking input voltage and dividing by two

--- AnalogTempTest/AnalogTempTest.ino
+++ AnalogTempTest/AnalogTempTest.ino
@@ -1,9 +1,27 @@
+int sensorPin=0;
 void setup() {
-  // put your setup code here, to run once:
-
+   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //getting the voltage reading from the temperature sensor
+ int reading = analogRead(sensorPin);  
+ 
+ // converting that reading to voltage, for 3.3v arduino use 3.3
+ float voltage = reading * 5.0;
+ voltage /= 1024.0; 
+ 
+ // print out the voltage
+ Serial.print(voltage); Serial.println(" volts");
+ 
+ // now print out the temperature
+ float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                               //to degrees ((voltage - 500mV) times 100)
+ Serial.print(temperatureC); Serial.println(" degrees C");
+ 
+ // now convert to Fahrenheit
+ float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+ Serial.print(temperatureF); Serial.println(" degrees F");
+ 
+ delay(1000); 
 }

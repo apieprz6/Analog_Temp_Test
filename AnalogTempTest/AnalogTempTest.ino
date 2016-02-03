663ba7e277a8fe6b467886caa2681fdd564d7d14
Alex Pieprzycki <alexpieprzycki@Alexs-MacBook-Pro.local>
2016-02-03 13:39:35 -0600

    Added all funcitonality to get the servo turning based on temperature that is sent into the arduino. However, there is something wrong with the serial communication, gets a different number than what I send to the arduino.

--- AnalogTempTest/AnalogTempTest.ino
+++ AnalogTempTest/AnalogTempTest.ino
@@ -1,26 +1,67 @@
+#include <Servo.h>
+
 int sensorPin=0;
+Servo servo;
+double recieved=0, staticTemp = 72, threshold = 1;
+bool change=false;
+
 
 double Thermistor(int RawADC) {
-double Temp;
-Temp = log(((10240000/RawADC) - 10000));
-Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
-Temp = Temp - 273.15;           
- return Temp;
+  double Temp;
+  Temp = log(((10240000/RawADC) - 10000));
+  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
+  Temp = Temp - 273.15;           
+  return Temp;
+}
+bool checkChange(double temp){
+  if(staticTemp == recieved){
+    return false;
+  }
+  else{
+    staticTemp = recieved;
+    return true;
+  }
 }
 void setup() {
    Serial.begin(9600);
+   servo.attach(9);
+   servo.write(0);
+   delay(1000);
+   servo.detach();
 }
 
 void loop() {
+  if(Serial.available() >0){
+    recieved = Serial.read();  
+    Serial.print("RECIEVED: ");
+    Serial.println(recieved);
+  }
   //getting the voltage reading from the temperature sensor
  int reading = analogRead(sensorPin);  
  double temp =  Thermistor(reading);
  // print out the voltage
- Serial.println("Temp. : ");
+ /*Serial.println("Temp. : ");
  Serial.print(temp); 
  Serial.println(" Celsius");
- double fahren = temp * 9 / 5 +32;
+ */double fahren = temp * 9 / 5 +32;/*
  Serial.print(fahren);
- Serial.println(" Fahrenheit");
- delay(1000); 
+ Serial.println(" Fahrenheit");*/
+ change = checkChange(staticTemp);
+ if(change){
+    if(fahren > staticTemp){
+      servo.attach(9);
+      servo.write(180);
+      delay(1000);
+      servo.detach();
+    }
+    else if(fahren < staticTemp){
+      servo.attach(9);
+      servo.write(0);
+      delay(1000);
+      servo.detach();
+    }
+ }
+ Serial.print("is currently: ");
+ Serial.println(staticTemp);
+ delay(5000); 
 }

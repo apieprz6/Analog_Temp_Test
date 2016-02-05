b662c65c504d292f4c4d367bdab72ca366c2660a
Alex Pieprzycki <alexpieprzycki@Alexs-MacBook-Pro.local>
2016-02-05 13:36:43 -0600

    Removed debug print lines, added check to see if motor postitioned has been moved nd it adjusts the servo

--- AnalogTempTest/AnalogTempTest.ino
+++ AnalogTempTest/AnalogTempTest.ino
@@ -1,80 +1,41 @@
 #include <Servo.h>
 
-int sensorPin=0;
+int sensorPin = 0;
 Servo servo;
-double staticTemp = 72, threshold = 1, recievedFahren=0;
+int servoCheck = 0;
+double staticTemp = 0, threshold = 1, recievedFahren = 0;
 double recieved[2];
-int ASCII[10]={48,49,50,51,52,53,54,55,56,57};
-bool change=false;
-double fahren =0;
+int ASCII[10] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
+bool change = false;
+double fahren = 0;
 
 
 double Thermistor(int RawADC) {
   double Temp;
-  Temp = log(((10240000/RawADC) - 10000));
-  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
-  Temp = Temp - 273.15;           
+  Temp = log(((10240000 / RawADC) - 10000));
+  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp )) * Temp );
+  Temp = Temp - 273.15;
   return Temp;
 }
-bool checkChange(double temp){
- for(int i=0; i <2; i++){
-  if(staticTemp != recieved[0]+recieved[1]){
-    Serial.println("enter loop 1");
-    staticTemp = recieved[0]+recieved[1];
-    return true;
+
+bool checkChange(double temp) {
+  for (int i = 0; i < 2; i++) {
+    if (staticTemp != recieved[0] + recieved[1]) {
+      staticTemp = recieved[0] + recieved[1];
+      return true;
+    }
+    if (fahren > staticTemp && servo.read() != 180) {
+      return true;
+    }
+    else if (fahren < staticTemp && servo.read() != 0) {
+      return true;
+    }
+    return false;
   }
-  if(fahren > staticTemp && servo.read()!=180){
-    Serial.println("enter loop 2");
-    return true;
-  }
-  else if(fahren < staticTemp && servo.read()!=0){
-    Serial.println("enter loop 3");
-    return true;
-  }
-  Serial.println("no loop");
-  return false;
-}
-}
-void setup() {
-   Serial.begin(9600);
-   servo.attach(9);
-   servo.write(0);
-   delay(1000);
-   servo.detach();
 }
 
-void loop() {
-  if(Serial.available() >=2){
-    for(int i=0;i<2;i++){
-      recieved[i] = Serial.read();
-    }
-    for(int i=0;i<2;i++){
-    Serial.print("RECIEVED: ");
-    Serial.println(recieved[i]);
-    }
-    for(int a=0;a<2;a++){
-      for(int j=0;j<10;j++){
-        if(ASCII[j]==recieved[a]){
-          recieved[a]=j;
-        }
-      }
-    }
-    recieved[0]=recieved[0]*10;
-    recievedFahren=recieved[0]+recieved[1];
-  }
-  //getting the voltage reading from the temperature sensor
- int reading = analogRead(sensorPin);  
- double temp =  Thermistor(reading);
- // print out the voltage
- /*Serial.println("Temp. : ");
- Serial.print(temp); 
- Serial.println(" Celsius");
- */fahren = temp * 9 / 5 +32;
- Serial.print(fahren);
- Serial.println(" Fahrenheit");
- change = checkChange(staticTemp);
- if(change){
-    if(fahren >= staticTemp){
+void servoSet() {
+  if(fahren >= staticTemp){
       servo.attach(9);
       servo.write(180);
       delay(1000);
@@ -86,9 +47,55 @@ void loop() {
       delay(1000);
       servo.detach();
     }
+}
+void setup() {
+  Serial.begin(9600);
+  servo.attach(9);
+  servo.write(0);
+  delay(1000);
+  servo.detach();
+}
+
+void loop() {
+  if (Serial.available() >= 2) {
+    for (int i = 0; i < 2; i++) {
+      recieved[i] = Serial.read();
+    }
+    for (int i = 0; i < 2; i++) {
+      Serial.print("RECIEVED: ");
+      Serial.println(recieved[i]);
+    }
+    for (int a = 0; a < 2; a++) {
+      for (int j = 0; j < 10; j++) {
+        if (ASCII[j] == recieved[a]) {
+          recieved[a] = j;
+        }
+      }
+    }
+    recieved[0] = recieved[0] * 10;
+    recievedFahren = recieved[0] + recieved[1];
+  }
+  //getting the voltage reading from the temperature sensor
+  int reading = analogRead(sensorPin);
+  double temp =  Thermistor(reading);
+  // print out the voltage
+  /*Serial.println("Temp. : ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+  */fahren = temp * 9 / 5 + 32;
+  Serial.print(fahren);
+  Serial.println(" Fahrenheit");
+  change = checkChange(staticTemp);
+  if (change) {
+    servoSet();
     change = false;
- }
- Serial.print("Set Temperature: ");
- Serial.println(staticTemp);
- delay(2000); 
+  }
+  Serial.print("Set Temperature: ");
+  Serial.println(staticTemp);
+  if (servoCheck > 10) {
+    servoSet();
+    servoCheck = 0;
+  }
+  servoCheck++;
+  delay(2000);
 }
